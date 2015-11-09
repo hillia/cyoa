5b770128de69426b9233e71d0327e8e6c5e857a8
hillia <khaemera@gmail.com>
2015-11-09 01:37:00 -0800

    Fixing routing.

--- cyoa.jsx
+++ cyoa.jsx
@@ -3,9 +3,9 @@ const {IndexRoute, Route} = ReactRouter;
 Chapters = new Mongo.Collection("chapters");
 
 AppRoutes = (
-  <Route path="/" component={App}>
-    <IndexRoute component={ChapterList} />
-  </Route>
+    <Route path="/" component={App}>
+        <IndexRoute component={ChapterList} />
+    </Route>
 );
 
 ReactRouterSSR.Run(AppRoutes);
